05900d318605e7bd053895ba9639be644a10f4d9
Ronnen Nagal <nagalr@gmail.com>
2021-01-20 16:54:21 +0200

    Create the Course.java class with @ManyToOne relation to the Instructor class

--- src/main/java/com/example/domain/Course.java
+++ src/main/java/com/example/domain/Course.java
@@ -1,7 +1,6 @@
 package com.example.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 /**
  * Created by ronnen on 20-Jan-2021
@@ -11,5 +10,59 @@ import javax.persistence.Table;
 @Table(name = "course")
 public class Course {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
 
+    @Column(name = "title")
+    private String title;
+
+    @ManyToOne(cascade = {CascadeType.DETACH,
+                            CascadeType.MERGE,
+                            CascadeType.PERSIST,
+                            CascadeType.REFRESH} )
+    @JoinTable(name = "instructor_id")
+    private Instructor instructor;
+
+    public Course() {
+    }
+
+    public Course(String title, Instructor instructor) {
+        this.title = title;
+        this.instructor = instructor;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", instructor=" + instructor +
+                '}';
+    }
 }
